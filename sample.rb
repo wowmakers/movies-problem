295162cfe726ed97cf821a1a770fc920f4769a94
Nithin Bekal <nithinbekal@gmail.com>
2012-07-21 18:40:05 +0530

    Clean up the comments in the sample file

--- sample.rb
+++ sample.rb
@@ -1,14 +1,15 @@
 
 # This is a sample file with only the outline of the program to get you
-# started if you're using Ruby. Edit this file and writing 
+# started if you're using Ruby. Edit this file and modify the code to get the
+# correct output.
 
-require 'json' # `gem install json` to get this to work
+require 'json' # `gem install json` if you don't already have json gem installed
 
 raw_json = IO.read('data/movies.json')
 
 # Parse the JSON data to get movie details
 
-genres = ['Thriller']   # => load all the unique genre names (sorted)
+genres = ['Thriller']   # Collect the genre names here.
 
 # Print the ratings and genre names
 genres.each do |genre|
